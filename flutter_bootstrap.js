17d54886d0270b5bb737ae4548b2124a71120bfe
m-adamantini <m.adamantini@dekosrl.com>
2025-04-04 11:26:53 +0200

    pub

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "414484608"
+    serviceWorkerVersion: "2462750226"
   }
 });
